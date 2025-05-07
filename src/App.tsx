304db0ad68fc3970aacdbda0ede9783917e489ea
devwanshitong <devwanshitong@gmail.com>
2025-05-07 05:02:43 -0500

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-function App() {
-  const [count, setCount] = useState(0);
+import { useEffect, useState } from "react";
+import "./App.css";
 
-  return (
-    <>
-      <button
-        className="custom-button"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        count is {count}
-      </button>
-    </>
-  );
+function App() {
+  const [data, setData] = useState([]);
+  const [loading, serLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const consoleLoader = (loadingValue: boolean) => {
+    serLoading(loadingValue);
+    console.info(loading);
+  };
+
+  const fetchData = async () => {
+    consoleLoader(true);
+
+    try {
+      const response = await fetch("https:///api.example.com/data");
+      if (!response.ok) {
+        throw new Error("Error al obtener datos");
+      }
+
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (err) {
+      setError(err as string);
+    } finally {
+      consoleLoader(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Cargando ....</div>;
+  }
+
+  if (error) {
+    return <div>UPS! Hay un error: {error}</div>;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;
