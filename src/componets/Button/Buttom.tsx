d27d15d482a2699939a0fa45638594055531d3f8
devwanshitong <devwanshitong@gmail.com>
2025-06-05 19:06:31 -0500

    composition pattern 1

--- src/componets/Button/Buttom.tsx
+++ src/componets/Button/Buttom.tsx
@@ -1,9 +1,16 @@
+import { sharedValueChildren } from "../../services/share-value-children.services";
 import "./Button.css";
 
 interface Props {
   label: string;
   parentMethod: () => void;
 }
+
+export const ChildrenButton = ({ label }: Pick<Props, "label">) => {
+  const services = sharedValueChildren;
+
+  return <div>{services.getValue()} </div>;
+};
 
 export const Buttom = ({ label, parentMethod }: Props) => {
   return (
---
+++ src/services/share-value-children.services.ts
@@ -0,0 +1,22 @@
+export class SharedValueChildren {
+  private value: string;
+
+  constructor(value: string) {
+    this.value = value;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  setValue(value: string) {
+    if (value.length < 1) {
+      throw new Error(
+        "value has to be a string and has have at least a character",
+      );
+    }
+    this.value = value;
+  }
+}
+
+export const sharedValueChildren = new SharedValueChildren("");
